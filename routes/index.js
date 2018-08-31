36419a8fae48d45e2818e7105f65b71fdb897cfb
Ratko Korle <ratko_korle@hotmail.com>
2018-08-31 12:23:19 +0200

    shopping-cart, checout view

--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,43 @@ router.get('/', function(req, res, next) {
   });
 });
 
-router.get('/add-to-cart/:id', function (req, res, next) {
+router.get('/add-to-cart/:id', function(req, res, next) {
     let productId = req.params.id;
-    let cart = new Cart(req.session.cart ? req.session.cart.items : {});
+    let cart = new Cart(req.session.cart ? req.session.cart : {});
 
-    Product.findById(productId, function (err, product) {
+    Product.findById(productId, function(err, product) {
+        if (err) {
+            return res.redirect('/');
+        }
         cart.add(product, product.id);
         req.session.cart = cart;
+        console.log(req.session.cart);
         res.redirect('/');
     });
+});
+router.get('/shopping-cart', function(req, res, next) {
+    if (!req.session.cart) {
+        return res.render('shop/shopping-cart', {products: null});
+    }
+    let cart = new Cart(req.session.cart);
+    res.render('shop/shopping-cart', {products: cart.generateArray(), totalPrice: cart.totalPrice});
+});
+router.get('/checkout', isLoggedIn, function(req, res, next) {
+    if (!req.session.cart) {
+        return res.redirect('/shopping-cart');
+    }
+    let cart = new Cart(req.session.cart);
+    let errMsg = req.flash('error')[0];
+    res.render('shop/checkout', {total: cart.totalPrice, errMsg: errMsg, noError: !errMsg});
 });
 
 
 module.exports = router;
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    req.session.oldUrl = req.url;
+    res.redirect('/user/signin');
+}
